1bcfac3e6190e2271840da28dcef47e310d76346
Mio Yasutake <mio.fujita.01@gmail.com>
2023-09-17 19:57:08 +0000

    correct test

--- tests/http.test.js
+++ tests/http.test.js
@@ -5,8 +5,8 @@ describe('my test suite', ()=>{
   const { GET, expect } = cds.test(project)
   it ('should test', async ()=>{ // Jest & Mocha style
     const { data } = await GET ('/odata/v4/catalog/Books', {
-      params: { $filter: 'ID eq 1' }
+      // params: { $filter: 'ID eq 1' }
     })
-    expect(data.value.length).to.eql(1)
+    expect(data.value.length).to.eql(2)
   })
 })
